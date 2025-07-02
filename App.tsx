ffc8ccfd3c8d12ee25fa161c4416893e7fe16ce3
bae <177719587+betapix@users.noreply.github.com>
2025-07-02 15:06:31 +0500

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { ScreenContent } from 'components/ScreenContent';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+import './global.css';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+    </View>
+  );
+}
